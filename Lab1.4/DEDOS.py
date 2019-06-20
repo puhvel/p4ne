06cb7509e16292fbbc1f2f0e18fdc8e5328c5de2
puhvel <puhvel@mail.ru>
2019-06-20 18:07:13 +0300

    4 lab

---
+++ Lab1.4/DEDOS.py
@@ -0,0 +1,12 @@
+import ipaddress
+import random
+
+class IPv4RandomNetwork(ipaddress.IPv4Network):
+        def __init__(self, ):
+
+        def regular(self):
+            return True if ipaddress.IPv4Address.is_global
+        # key_value - ip + mask - for further sorting
+        def key_value(self):
+            return
+
