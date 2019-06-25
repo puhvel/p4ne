4934dc2ad16b30739c9388cbd905032212c76c67
puhvel <puhvel@mail.ru>
2019-06-25 16:38:40 +0300

    4 lab

--- Lab1.4/DEDOS.py
+++ Lab1.4/DEDOS.py
@@ -1,11 +1,21 @@
 import ipaddress
 import random
 
+min_net = '0B.00.00.00'
+max_net = 'DF.00.00.00'
+print(max_net.split('.'))
+
+
+print(int(max_net))
+
 class IPv4RandomNetwork(ipaddress.IPv4Network):
-        def __init__(self, ):
+    def __init__(self):
+        self.network_address = random.ranint
+        self.netmask = p
+        addr = ipaddress.IPv4Address(())
 
         def regular(self):
-            return True if ipaddress.IPv4Address.is_global
+            return ipaddress.IPv4Address.is_global)
         # key_value - ip + mask - for further sorting
         def key_value(self):
             return
