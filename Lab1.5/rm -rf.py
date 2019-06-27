55ccc329609260725ffe16eb1e96848f3a08abd9
puhvel <puhvel@mail.ru>
2019-06-27 15:27:18 +0300

    1.5

--- Lab1.5/rm -rf.py
+++ Lab1.5/rm -rf.py
@@ -1,1 +1,18 @@
-#C:\Users\Dre\Seafile\p4ne_training\config_files
+import glob
+
+files = (glob.glob("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\*.txt"))
+strings_file = open("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\substrings.txt", 'a')
+
+#for i in file_dir:
+ #   print(i)
+
+for file in files:
+    f_open = open(file)
+    for line in f_open:
+        if i = str(line).find("ip address "):
+            strings_file.write(i)
+    f_open.close()
+    strings_file.write("")
+    count = count+1
+
+print(count)
