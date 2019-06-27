656d220d80f826ef654e42d085785f847e760055
puhvel <puhvel@mail.ru>
2019-06-27 17:15:15 +0300

    1.5

--- Lab1.5/rm-rf.py
+++ Lab1.5/rm-rf.py
@@ -1,7 +1,7 @@
 import glob
 
 files = (glob.glob("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\*.txt"))
-substr_f = open("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\substrings.txt", 'a')
+substr_f = list(open("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\substrings.txt", 'a'))
 
 count=0
 
@@ -9,8 +9,7 @@ for file in files:
     f_open = open(file)
     for line in f_open:
         substr_s = str(line).find("ip address ")
-        if substr_s:
-            substr_f.write(substr_s)
+        if substr_s            substr_f.write(substr_s)
     f_open.close()
     substr_f.write("")
     substr_f.write(count+1)
--- Lab1.5/ver2.py
+++ Lab1.5/ver2.py
@@ -1,19 +1,16 @@
 import glob
 
-file_dir = (glob.glob("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\*.txt"))
-strings_file = open("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\substrings.txt", 'a')
+files = (glob.glob("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\*.txt"))
+substrings_file = open("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\substrings.txt", 'a')
+c=0
+for file in files:
+    f = open(file)
+    c=c+1
+    for line in f:
+        sub_str = line.find('ip address ')
+        if sub_str:
+            substrings_file.write(line)
+    f.close()
 
-#for i in file_dir: print(i)
-
-for i_file in file_dir:
-    for i_str in i_file:
-        j = str(i_file(i_str))
-        if j in i_str:
-            j = i_str.find("ip address ")
-
-    i_file.close()
-
-# strings.close()
-print(strings_file)
-
-# l = [i_string.strip() for line in f]
+print(c)
+#substrings.write(sub_str)
