f58a22b06fad86dd05fbccdc4fecbd10de39fb28
puhvel <puhvel@mail.ru>
2019-06-27 15:46:55 +0300

    1.5

--- Lab1.5/rm-rf.py
+++ Lab1.5/rm-rf.py
@@ -6,15 +6,17 @@ strings_file = open("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\subst
 # for i in file_dir:
 #   print(i)
 
+count=0
+
 for file in files:
     f_open = open(file)
     for line in f_open:
         for str_find in line:
-            if str(line).find("ip address "):
+            str(line).find("ip address ")==True:
                 strings_file.write(str_find)
     f_open.close()
     strings_file.write("")
-    count = count + 1
+    count=count+1
 
-print(count)
 strings_file.close()
+print(count)
