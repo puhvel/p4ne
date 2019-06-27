d82942da8b43139174a642bfdef72ae8b64757cb
puhvel <puhvel@mail.ru>
2019-06-27 18:06:32 +0300

    1.5

--- Lab1.5/ver2.py
+++ Lab1.5/ver2.py
@@ -1,16 +1,22 @@
 import glob
 
 files = (glob.glob("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\*.txt"))
-substrings_file = open("C:\\Users\\Dre\\Seafile\\p4ne_training\\config_files\\substrings.txt", 'a')
-c=0
+substrings_file = open("C:\\Users\\Dre\\Seafile\\p4ne_training\\substrings.txt",'r+')
+
 for file in files:
     f = open(file)
-    c=c+1
     for line in f:
         sub_str = line.find('ip address ')
-        if sub_str:
+        if sub_str != -1:
             substrings_file.write(line)
     f.close()
 
-print(c)
-#substrings.write(sub_str)
+mylist = []
+substrings_file.seek(0)
+
+for i in substrings_file: mylist.append[i]
+print(mylist)
+
+
+#убрать лишние и повторяющиеся значения из собраных данных
+#сортировать по ip и маске
